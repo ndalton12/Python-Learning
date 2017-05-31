0221520d1caf447be40d3ae9257b0ee3d4c7cdb9
Niall <niall.dalton12@gmail.com>
2017-05-30 20:33:51 -0400

    Finished newcoderio tutorial 1

---
+++ newcoderio/DataViz/parse.py
@@ -0,0 +1,40 @@
+import csv
+
+MY_FILE = "sample_sfpd_incident_all.csv"
+
+def parse(raw_file, delimiter):
+	"""Parses a raw CSV file to a JSON-line object"""
+	
+	# Open CSV file
+	opened_file = open(raw_file)
+
+	# Read CSV file
+	csv_data = csv.reader(opened_file, delimiter=delimiter)
+	
+	# Init data
+	parsed_data = []
+	
+	# Header data
+	fields = next(csv_data)
+
+	# Append CSV info into data
+	for row in csv_data:
+	        parsed_data.append(dict(zip(fields, row)))
+
+	# Close CSV file	
+	opened_file.close()
+
+	# Build data
+
+	
+	return parsed_data
+
+def main():
+	# Call parse function
+	new_data = parse(MY_FILE, ',')
+
+	# Output
+	print(new_data)
+
+if __name__ == '__main__':
+	main()
